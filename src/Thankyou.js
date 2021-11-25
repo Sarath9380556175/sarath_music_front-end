6275d202635581023cd533d9f5f4335a5012da94
Bujala sarath kumar reddy <75170692+Sarath9380556175@users.noreply.github.com>
2021-11-25 20:27:03 +0530

    Update Thankyou.js

--- src/Thankyou.js
+++ src/Thankyou.js
@@ -25,7 +25,7 @@ class Thankyou extends React.Component{
     {
         return(
             <div>
-                <div className="mt-3 ml-2 text-white" onClick={this.back}>BACK</div>
+                <div className="mt-3 ml-2 text-success" onClick={this.back}>BACK</div>
 <Zoom cascade bottom><div className="mt-3 text-center text-white">
                 Successfull
             </div></Zoom>
